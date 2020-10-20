b1fefb755fd5578f1ddccf565574985a45ec0261
turbq <gron069@gmail.com>
2020-10-20 19:17:10 +0300

    everything works fine

--- src/brightness.c
+++ src/brightness.c
@@ -6,14 +6,16 @@
 
 #include "hidapi.h"
 
-#define MAX_STR 255
+#define VENDOR_ID ((unsigned short) 0x03eb)
+#define PRODUCT_ID ((unsigned short) 0x2402)
 
 int main(int argc, char *argv[])
 {
     int res;
 	unsigned char buf[65];
-	wchar_t wstr[MAX_STR];
 	hid_device *handle;
+    struct hid_device_info *devs, *cur_dev;
+    char *mtouch_path;
 
 	if(argc!=2){
 		printf("no argument\n");
@@ -26,30 +28,39 @@ int main(int argc, char *argv[])
 	res = hid_init();
 	if (res)
 		return -1;
+    devs = hid_enumerate(VENDOR_ID, PRODUCT_ID);
+    if(devs == NULL){
+		printf("Devices with type: %04hx:%04hx not found\n", VENDOR_ID, PRODUCT_ID);
+        return -1;
+    }
+	cur_dev = devs;
+	while (cur_dev) {
+        if((cur_dev->vendor_id == VENDOR_ID)&&(cur_dev->product_id == PRODUCT_ID)&&(cur_dev->interface_number == 3)){
+            printf("Device Found\n  type: %04hx:%04hx\n  path: %s\n  serial_number: %ls", cur_dev->vendor_id, cur_dev->product_id, cur_dev->path, cur_dev->serial_number);
+            printf("\n");
+            printf("  Manufacturer: %ls\n", cur_dev->manufacturer_string);
+            printf("  Product:      %ls\n", cur_dev->product_string);
+            printf("  Release:      %hx\n", cur_dev->release_number);
+            printf("  Interface:    %d\n",  cur_dev->interface_number);
+            printf("  Usage (page): 0x%hx (0x%hx)\n", cur_dev->usage, cur_dev->usage_page);
+            printf("\n");
+            mtouch_path = malloc(sizeof(cur_dev->path));
+            if(mtouch_path == NULL)
+                return -1;
+            strcpy(mtouch_path, cur_dev->path);
+        }
+		cur_dev = cur_dev->next;
+	}
+	hid_free_enumeration(devs);
 
 	// Open the device using the VID, PID,
 	// and optionally the Serial number.
-	handle = hid_open(0x03eb, 0x2402, NULL);
+    handle = hid_open_path(mtouch_path);
+    free(mtouch_path);
 	if(!handle) {
 		printf("unable to open device\n");
 		return 1;
 	}
-
-	// Read the Manufacturer String
-	res = hid_get_manufacturer_string(handle, wstr, MAX_STR);
-	printf("Manufacturer String: %S\n", wstr);
-
-	// Read the Product String
-	res = hid_get_product_string(handle, wstr, MAX_STR);
-	printf("Product String: %S\n", wstr);
-
-	// Read the Serial Number String
-	res = hid_get_serial_number_string(handle, wstr, MAX_STR);
-	printf("Serial Number String: (%d) %S\n", wstr[0], wstr);
-
-	// Read Indexed String 1
-	res = hid_get_indexed_string(handle, 1, wstr, MAX_STR);
-	printf("Indexed String 1: %S\n", wstr);
 
 	memset(buf,0x00,sizeof(buf));
 
@@ -63,24 +74,7 @@ int main(int argc, char *argv[])
 		printf("Unable to write()\n");
 		printf("hid error: %ls\n", hid_error(handle));
 	}	
-
-	// Toggle LED (cmd 0x80). The first byte is the report number (0x0).
-	//buf[0] = 0x0;
-	//buf[1] = 0x80;
-	//res = hid_write(handle, buf, 65);
-
-	// Request state (cmd 0x81). The first byte is the report number (0x0).
-	//buf[0] = 0x0;
-	//buf[1] = 0x81;
-	//res = hid_write(handle, buf, 65);
-
-	// Read requested state
-	//res = hid_read(handle, buf, 65);
-
-	// Print out the returned buffer.
-	//for (i = 0; i < 4; i++)
-		//printf("buf[%d]: %d\n", i, buf[i]);
-
+    printf("success\n");
 	// Close the device
 	hid_close(handle);
 
